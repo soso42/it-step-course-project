65eabe7397814c0a4395602c99ca5c3aeac5b514
Soso42 <sosojanashvili@gmail.com>
2024-06-19 18:37:34 +0400

    added header and footer components

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../../shared/footer/footer.component";
+import { HeaderComponent } from "../../shared/header/header.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    FooterComponent,
+    HeaderComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ src/app/shared/info-cards/info-cards.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-cards',
+  standalone: true,
+  imports: [],
+  templateUrl: './info-cards.component.html',
+  styleUrl: './info-cards.component.scss'
+})
+export class InfoCardsComponent {
+
+}
