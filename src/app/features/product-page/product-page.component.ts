55f6ab93bfd4aac802912032a596e1edeebb3dfe
Soso42 <sosojanashvili@gmail.com>
2024-06-20 20:06:52 +0400

    Finished product-page

--- src/app/features/product-page/product-page.component.ts
+++ src/app/features/product-page/product-page.component.ts
@@ -1,7 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, inject, Renderer2, ViewChild} from '@angular/core';
 import { HeaderComponent } from "../../shared/header/header.component";
 import { FooterComponent } from "../../shared/footer/footer.component";
 import { InfoCardsComponent } from "../../shared/info-cards/info-cards.component";
+import { ActivatedRoute } from "@angular/router";
+import { ProductService } from "../../services/product.service";
+import { Product } from "../../interfaces/product";
+import { catchError, tap } from "rxjs";
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-product-page',
@@ -9,11 +14,58 @@ import { InfoCardsComponent } from "../../shared/info-cards/info-cards.component
   imports: [
     HeaderComponent,
     FooterComponent,
-    InfoCardsComponent
+    InfoCardsComponent,
+    FormsModule
   ],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.scss'
 })
 export class ProductPageComponent {
+
+  @ViewChild('productMainImage') mainImage!: ElementRef;
+
+  activatedRoute = inject(ActivatedRoute);
+  productService = inject(ProductService);
+  renderer = inject(Renderer2);
+
+  product!: Product;
+  productQuantityToBuy!: number;
+
+  ngOnInit() {
+
+    const product_id = this.activatedRoute.snapshot.params['id'];
+
+    this.productService.getProductById(product_id)
+      .pipe(
+        tap(result => {
+          this.product = result;
+
+          this.productQuantityToBuy = this.product.stock > 1 ? 1 : 0;
+          console.log(this.product)
+
+        }),
+        catchError(err => {
+          throw 'Error:' + err;
+        })
+      )
+      .subscribe();
+
+  }
+
+  onClickThumbnail(imageURL: string) {
+    this.renderer.setAttribute(this.mainImage.nativeElement, 'src', imageURL);
+  }
+
+  onClickDecrement() {
+    if (this.productQuantityToBuy > 1) {
+      this.productQuantityToBuy--;
+    }
+  }
+
+  onClickIncrement() {
+    if (this.product.stock > this.productQuantityToBuy) {
+      this.productQuantityToBuy++;
+    }
+  }
 
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { ProductResponse } from "../interfaces/product-response";
+import { Product } from "../interfaces/product";
 
 
 @Injectable({
@@ -15,6 +16,11 @@ export class ProductService {
   getProducts() {
     return this.httpClient
       .get<ProductResponse>('https://api.everrest.educata.dev/shop/products/all?page_index=1&page_size=15');
+  }
+
+  getProductById(id: string) {
+    return this.httpClient
+      .get<Product>('https://api.everrest.educata.dev/shop/products/id/' + id);
   }
 
 }
