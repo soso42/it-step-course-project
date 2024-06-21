3c75ddcb80ee137a8ed2e3116d9b603f5865d83c
Soso42 <sosojanashvili@gmail.com>
2024-06-21 15:19:16 +0400

    completed contact us form

--- src/app/features/contact/contact.component.ts
+++ src/app/features/contact/contact.component.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "../../shared/footer/footer.component";
 import { HeaderComponent } from "../../shared/header/header.component";
+import { FormsModule, NgForm } from "@angular/forms";
+import { NgClass } from "@angular/common";
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    FormsModule,
+    NgClass
   ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+
+  onClickSubmit(contactForm: NgForm) {
+    console.log(contactForm.form.controls['name'].value);
+    console.log(contactForm.form.controls['email'].value);
+    console.log(contactForm.form.controls['text'].value);
+
+    contactForm.form.reset();
+  }
 
 }
